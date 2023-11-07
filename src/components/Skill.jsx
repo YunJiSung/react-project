070df82dea6c93f79c686432e8ea8f7f7111f37b
(YunJiSung) <(capitaljs@gmail.com)>
2023-11-07 13:17:23 +0900

    vv

---
+++ src/components/Skill.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Skill = () => {
+    return (
+        <section id="skill">
+            <div className="skill__inner">
+                <h2 className="skill__title">
+                    challenge <em>나의 도전</em>
+                </h2>
+                <div className="skill__desc">
+                    <div>
+                        <span>1.</span>
+                        <h3>꿈을 설계하고 디자인하다.</h3>
+                        <p>
+                            자신은 할 수 없다고 생각하는 동안
+                            사실은 그것을 하기 싫다고
+                            다짐하고 있는 것이다.
+                            그러므로 실행되지 않는 것이다.
+                        </p>
+                    </div>
+                    <div>
+                        <span>2.</span>
+                        <h3>꿈을 설계하고 디자인하다.</h3>
+                        <p>
+                            내가 바꿀수 있는 것은 바꿀 수 있는 용기를 가지고, 바꿀 수 없는 것에 대해서는
+                            받아들일 수 있는 인내를 가지고, 이 둘을 분별할 수 있는 지혜를 가져야 한다.
+                        </p>
+                    </div>
+                    <div>
+                        <span>3.</span>
+                        <h3>꿈을 설계하고 디자인하다.</h3>
+                        <p>
+                            내가 바꿀수 있는 것은 바꿀 수 있는 용기를 가지고, 바꿀 수 없는 것에 대해서는
+                            받아들일 수 있는 인내를 가지고, 이 둘을 분별할 수 있는 지혜를 가져야 한다.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Skill
---
+++ src/components/Skip.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skip = () => {
+    return (
+        <div id="skip">
+            <a href="#header">헤더 영역 바로가기</a>
+            <a href="#intro">헤더 영역 바로가기</a>
+            <a href="#skill">헤더 영역 바로가기</a>
+            <a href="#site">헤더 영역 바로가기</a>
+            <a href="#port">헤더 영역 바로가기</a>
+            <a href="#contact">헤더 영역 바로가기</a>
+            <a href="#footer">헤더 영역 바로가기</a>
+        </div>
+    )
+}
+
+export default Skip
